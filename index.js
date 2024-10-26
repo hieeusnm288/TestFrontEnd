ddb4ad6c06f1c507a298c457ead411d182d004c5
Nguyen Hieu <nguyenminhhieu28801@gmail.com>
2024-10-26 11:47:06 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+const targetDate = new Date("2024-12-31T23:59:59");
+
+const updateCountdown = () => {
+  const now = new Date();
+  const timeDifference = targetDate - now;
+
+  if (timeDifference > 0) {
+    const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+    const hours = Math.floor(
+      (timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    );
+    const minutes = Math.floor(
+      (timeDifference % (1000 * 60 * 60)) / (1000 * 60)
+    );
+    const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
+
+    document.getElementById("days").textContent = days
+      .toString()
+      .padStart(2, "0");
+    document.getElementById("hours").textContent = hours
+      .toString()
+      .padStart(2, "0");
+    document.getElementById("minutes").textContent = minutes
+      .toString()
+      .padStart(2, "0");
+    document.getElementById("seconds").textContent = seconds
+      .toString()
+      .padStart(2, "0");
+  } else {
+    document.getElementById("countdownSection").textContent =
+      "Countdown Finished!";
+  }
+};
+setInterval(updateCountdown, 1000);
+
+updateCountdown();
